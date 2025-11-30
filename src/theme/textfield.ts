74f122b085dfb6e6845e9e0a1b2a5283f7ec0f1b
Eduard Bryan P. Rivera <edubprivera@gmail.com>
2025-11-30 23:26:00 +0800

    Update theme to work with dark background

---
+++ src/theme/textfield.ts
@@ -0,0 +1,15 @@
+const TEXT_COLOR = "#C4CAC8"
+
+const textFieldTheme = {
+  root: {
+    label: {
+      color: TEXT_COLOR
+    },
+    input: {
+      color: TEXT_COLOR
+    },
+    borderBottom: `solid 1px ${TEXT_COLOR}`
+  }
+}
+
+export default textFieldTheme
